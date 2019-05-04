70a2fe644c55d67f85f73742577eefb1fec57b53
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-04 20:55:57 +0000

    setup connection to mongoDB which is on mLab

--- books/books.js
+++ books/books.js
@@ -1,6 +1,14 @@
 //Load Express
 const express = require('express')
 const app = express();
+//Load Mongoose
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://testuser:pass1234\$@ds149596.mlab.com:49596/nodejs_microservices", {useNewUrlParser: true}).then(() => {
+    console.log("Database is connected")
+},
+err => {
+    console.log("There was an error connecting to mLab", err)
+})
 
 
 app.get('/', (req, res) => {
