193869acb2f6b450b110e906b4994c0bd9355758
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-04 23:47:50 +0000

    create the certs and private directory only if they don't currently exist

--- vagrant/nginx.sh
+++ vagrant/nginx.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+dir="/etc/nginx/certs"
+
+if [[ ! -e $dir ]]; then
+    sudo mkdir $dir
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+
+dir1="/etc/nginx/private"
+
+if [[ ! -e $dir1 ]]; then
+    sudo mkdir $dir1
+elif [[ ! -d $dir1 ]]; then
+    echo "$dir1 already exists but is not a directory" 1>&2
+fi
+
 if [ ! -e /var/www/project/nginxnginx ]
 then
 
@@ -16,26 +32,26 @@ then
 
 	# only run once
 	touch /var/www/project/nginxnginx
-	
+
 	sudo cp /var/www/project/vagrant/certs/books.node.app.crt /etc/nginx/certs/books.node.app.crt
-	sudo cp /var/www/project/vagrant/certs/books.node.app.key /etc/nginx/certs/books.node.app.key
+	sudo cp /var/www/project/vagrant/certs/books.node.app.key /etc/nginx/private/books.node.app.key
 
 	sudo cp /var/www/project/vagrant/certs/customers.node.app.crt /etc/nginx/certs/customers.node.app.crt
-	sudo cp /var/www/project/vagrant/certs/customers.node.app.key /etc/nginx/certs/customers.node.app.key
+	sudo cp /var/www/project/vagrant/certs/customers.node.app.key /etc/nginx/private/customers.node.app.key
 
 	sudo cp /var/www/project/vagrant/certs/orders.node.app.crt /etc/nginx/certs/orders.node.app.crt
-	sudo cp /var/www/project/vagrant/certs/orders.node.app.key /etc/nginx/certs/orders.node.app.key
+	sudo cp /var/www/project/vagrant/certs/orders.node.app.key /etc/nginx/private/orders.node.app.key
 
 else
 
 	echo ">>> nginx already setup..."
 	sudo cp /var/www/project/vagrant/certs/books.node.app.crt /etc/nginx/certs/books.node.app.crt
-	sudo cp /var/www/project/vagrant/certs/books.node.app.key /etc/nginx/certs/books.node.app.key
+	sudo cp /var/www/project/vagrant/certs/books.node.app.key /etc/nginx/private/books.node.app.key
 
 	sudo cp /var/www/project/vagrant/certs/customers.node.app.crt /etc/nginx/certs/customers.node.app.crt
-	sudo cp /var/www/project/vagrant/certs/customers.node.app.key /etc/nginx/certs/customers.node.app.key
+	sudo cp /var/www/project/vagrant/certs/customers.node.app.key /etc/nginx/private/customers.node.app.key
 
 	sudo cp /var/www/project/vagrant/certs/orders.node.app.crt /etc/nginx/certs/orders.node.app.crt
-	sudo cp /var/www/project/vagrant/certs/orders.node.app.key /etc/nginx/certs/orders.node.app.key
+	sudo cp /var/www/project/vagrant/certs/orders.node.app.key /etc/nginx/private/orders.node.app.key
 
 fi
