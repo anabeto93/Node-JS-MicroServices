65f14d46cdb21c33dcf774a7b81b55b810d0f190
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-05 02:35:28 +0000

    created the base route of the Order microservice

--- orders/orders.js
+++ orders/orders.js
@@ -15,3 +15,14 @@ mongoose.connect("mongodb://testuser:pass1234\$@ds149606.mlab.com:49606/nodejs_m
 err => {
     console.log("There was an error connecting to mLab", err)
 })
+
+//Base route
+app.get('/', (req, res) => {
+    console.log('Orders service')
+
+    res.send('Orders Service endpoint!')
+})
+
+app.listen(8003, () => {
+    console.log('Up and running - Orders service')
+})
