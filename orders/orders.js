8454efeb1b0251b3c8fa1b08c7fad25ba43c57b2
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-05 02:45:35 +0000

    corrected the Customer to Order, happened due to copy and paste

--- orders/orders.js
+++ orders/orders.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json())
 const mongoose = require('mongoose')
 //define the Customer
 require('./Order')
-const Customer = mongoose.model('Order')
+const Order = mongoose.model('Order')
 
 mongoose.connect("mongodb://testuser:pass1234\$@ds149606.mlab.com:49606/nodejs_microservices_orders", {useNewUrlParser: true}).then(() => {
     console.log("Orders Database is connected")
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 app.post('/books', (req, res) => {
     console.log('About creating a new order', req.body)
 
-    
+
 })
 
 app.listen(8003, () => {
