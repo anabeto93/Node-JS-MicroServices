5f2d5cfda6da42a103385fa57bd9b60d2fcf07cb
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-05 02:40:40 +0000

    mongoose to create collection for Orders

--- orders/orders.js
+++ orders/orders.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json())
 const mongoose = require('mongoose')
 //define the Customer
 require('./Order')
-//const Customer = mongoose.model('Order')
+const Customer = mongoose.model('Order')
 
 mongoose.connect("mongodb://testuser:pass1234\$@ds149606.mlab.com:49606/nodejs_microservices_orders", {useNewUrlParser: true}).then(() => {
     console.log("Orders Database is connected")
