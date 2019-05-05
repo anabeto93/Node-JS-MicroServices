3cba9e580c783430223f06bc7a54cf248090f8de
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-05 02:40:11 +0000

    order model defined

--- orders/Order.js
+++ orders/Order.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose")
 
 mongoose.model("Order", {
-    
+    CustomerId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+    },
+    BookId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+    },
+    dateBorrowed: {
+        type: Date,
+        required: true,
+    },
+    dateReturned: {
+        type: Date,
+        required: false,
+    }
 })
