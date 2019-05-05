2183207b8db80a6c870515d635f978ca032d8462
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-05 00:52:10 +0000

    endpoint to create a new book has been added

--- books/books.js
+++ books/books.js
@@ -4,8 +4,14 @@ const app = express();
 //Load Body Parser
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+
 //Load Mongoose
 const mongoose = require('mongoose')
+
+//Load Book Model
+require("./Book")
+const Book = mongoose.model("Book")
+
 mongoose.connect("mongodb://testuser:pass1234\$@ds149596.mlab.com:49596/nodejs_microservices", {useNewUrlParser: true}).then(() => {
     console.log("Database is connected")
 },
@@ -21,7 +27,28 @@ app.get('/', (req, res) => {
 //Create a book
 app.post('/books', (req, res) => {
     console.log('The request body received', req.body)
-    res.send("Testing book route!")
+    let newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        numberPages: req.body.numberPages,
+        publisher: req.body.publisher,
+    }
+
+    let book = new Book(newBook);
+
+    book.save().then(() => {
+        console.log("New book created!")
+    }).catch((err) => {
+        throw err;
+    })
+
+    data = {
+        status: 'created',
+        code: 201,
+        data: newBook
+    }
+
+    res.status(201).send(data)
 })
 
 app.listen(8001, () => {
