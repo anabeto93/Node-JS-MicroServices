739c182236b4390781d67857ac39ecd5f63169b4
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-05 01:41:48 +0000

    base endpoint of customers service created, running on https://customers.node.app

--- customers/customers.js
+++ customers/customers.js
@@ -4,14 +4,22 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
+const mongoose = require('mongoose')
 //define the Customer
 require('./Customer')
+const Customer = mongoose.model('Customer')
 
 mongoose.connect("mongodb://testuser:pass1234\$@ds149676.mlab.com:49676/nodejs_microservices_customers", {useNewUrlParser: true}).then(() => {
     console.log("Database is connected")
 },
 err => {
     console.log("There was an error connecting to mLab", err)
+})
+
+app.get('/', (req, res) => {
+    console.log('Customers service')
+
+    res.send('Customers Service endpoint!')
 })
 
 app.listen(8002, () => {
